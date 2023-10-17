30f5763a8a1c90fd3448291122547c361e65a985
weiserhase <44242102+weiserhase@users.noreply.github.com>
2023-10-18 00:00:00 +0200

    Fix Lualine Them to be Onedark

--- init.lua
+++ init.lua
@@ -181,7 +181,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = true,
-        theme = 'material-deep-ocean',
+        theme = 'onedark',
         component_separators = '|',
         section_separators = '',
       },
