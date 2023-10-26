2bff6aacdfb979c21b503d6df5f0d134017e75fa
weiserhase <44242102+weiserhase@users.noreply.github.com>
2023-10-26 13:48:57 +0200

    Keybinds Window (max, toggle vsp/hsp) + git push

--- lua/custom/keybindings/init.lua
+++ lua/custom/keybindings/init.lua
@@ -48,6 +48,7 @@ require('which-key').register({
 local gitKeybindings = {
     n = {
         ['<Leader>g'] = { cmd = ":G<CR>", desc = 'Run arbitrary Git command' },
+        ['<Leader>gp'] = { cmd = ":Git push", desc = '[G]it Push' },
     },
 }
 applyBindings(gitKeybindings)
@@ -82,11 +83,15 @@ local windowKeybindings = {
         ['<leader>wsh'] = { cmd = ':sp<CR>', desc = '[W]indow [H]orizontal split' },
         ['<leader>wq'] = { cmd = ':q<CR>', desc = '[W]indow [Q]uit split' },
         ['<leader>wh'] = { cmd = '<C-w>h', desc = '[W]indow move left' },
+        ['<leader>wH'] = { cmd = '<C-w>H', desc = '[W]indow move left' },
+        ['<leader>wK'] = { cmd = '<C-w>K', desc = '[W]indow move left' },
         ['<leader>wj'] = { cmd = '<C-w>j', desc = '[W]indow move down' },
         ['<leader>wk'] = { cmd = '<C-w>k', desc = '[W]indow move up' },
         ['<leader>wl'] = { cmd = '<C-w>l', desc = '[W]indow move right' },
         ['<leader>w+'] = { cmd = '<C-w>+', desc = '[W]indow increase height' },
         ['<leader>w-'] = { cmd = '<C-w>-', desc = '[W]indow decrease height' },
+        ['<leader>w_'] = { cmd = '<C-w>_', desc = '[W]indow Max height' },
+        ['<leader>w|'] = { cmd = '<C-w>|', desc = '[W]indow decrease height' },
         ['<leader>w<'] = { cmd = '<C-w><', desc = '[W]indow decrease width' },
         ['<leader>w>'] = { cmd = '<C-w>>', desc = '[W]indow increase width' },
     }
