bda6156bdc47682a101a743c39ddd1da9edf14af
weiserhase <j.keller.wol@gmail.com>
2024-11-06 21:18:30 +0100

    feat(update): idris config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -93,7 +93,22 @@ return {
         'ShinKage/idris2-nvim',
         dependencies = { 'neovim/nvim-lspconfig', 'MunifTanjim/nui.nvim' },
         opts = {
-            code_action_post_hook = save_hook
+            client = {
+                hover = {
+                    use_split = false,         -- Persistent split instead of popups for hover
+                    split_size = '30%',        -- Size of persistent split, if used
+                    auto_resize_split = false, -- Should resize split to use minimum space
+                    split_position = 'bottom', -- bottom, top, left or right
+                    with_history = false,      -- Show history of hovers instead of only last
+                    use_as_popup = false,      -- Close the split on cursor movement
+                },
+            },
+            code_action_post_hook = save_hook,
+
+            autostart_semantic = true,            -- Should start and refresh semantic highlight automatically
+            se_default_semantic_hl_groups = true, -- Set default highlight groups for semantic tokens
+            default_regexp_syntax = true,         -- Enable default highlight groups for traditional syntax based highlighting
+
         }
 
     },
